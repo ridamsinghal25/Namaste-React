d8666e879d8c2bb3565509ba8666a343251bc6cc
ridamsinghal25 <manusinghal84@gmail.com>
2024-06-23 07:36:51 +0530

    refactor Body component

--- Episode-9/src/components/About.js
+++ Episode-9/src/components/About.js
@@ -1,19 +1,27 @@
 import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
+import UserContext from "../utils/UserContext";
 
 class About extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {}
-
   render() {
+    console.log(this);
     return (
       <div>
         <h1>About Class Component</h1>
         <h2>Namaste React Episode 7</h2>
+        <div>
+          loggedIn User
+          <UserContext.Consumer>
+            {({ loggedInUser }) => (
+              <h1 className="text-xl font-bold">{loggedInUser}</h1>
+            )}
+          </UserContext.Consumer>
+        </div>
         <UserClass name={"first"} location="Nakur Class" />
       </div>
     );
